9eaca3e24aa01b7e5dc2d808e2610986ee5d1525
Blessing Fredy <blessingfsp@gmail.com>
2026-03-21 20:42:15 +0530

    created customer account entity and dto layer

---
+++ section2/src/main/java/com/easybytes/accounts/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.easybytes.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass //setting this class as superclass when extended by other classes
+@Getter @Setter  @ToString
+public class BaseEntity {
+
+    @Column(updatable = false) // for letting the compiler know that during update, this data must not be altered
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(insertable = false) // for letting the compiler know that on inserting new values, this column must not be updated
+    private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
+    private String updatedBy;
+
+}
